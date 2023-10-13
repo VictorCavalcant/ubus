09f4261195ce90e4b0a0c612c578684996510fac
VictorCavalcant <victorphb27@gmail.com>
2023-10-12 21:43:35 -0300

    Added Near Stops functionality

--- lib/pages/MapPage.dart
+++ lib/pages/MapPage.dart
@@ -5,7 +5,6 @@ import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:ubus/components/BottomMenu.dart';
-import 'package:ubus/consts.dart';
 import 'dart:ui' as ui;
 import 'package:ubus/data/stops.dart';
 import 'package:ubus/scripts/nearPoints.dart';
